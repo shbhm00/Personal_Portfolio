d533c146905d2fa8534047a116fd95b40b317fb1
shbhm0 <shubham.mishra@appinventiv.com>
2021-10-22 09:21:27 +0530

    updated about page

--- src/components/about.js
+++ src/components/about.js
@@ -2,6 +2,27 @@ const About = () => {
   return (
     <div className="about-wrapper">
       <h1 id="about"></h1>
+      <div>
+        <div className="about-intro">
+          <h2>
+            <span>Know Who</span> I'M
+          </h2>
+          <p>
+            Hi Everyone, I am <span>Shubham Mishra</span> from{" "}
+            <span>U P, India</span>. I am
+            <span>Mobile Application Developer</span>. Currently working for{" "}
+            <span>Appinventiv, Noida</span>.
+            <h4>Apart from coding, some other activities I love do.</h4>
+            <ul>
+              <li>Reading</li>
+              <li>Travelling</li>
+              <li>Writing quotes</li>
+              <li>Cooking</li>
+            </ul>
+          </p>
+        </div>
+        <div></div>
+      </div>
     </div>
   );
 };
