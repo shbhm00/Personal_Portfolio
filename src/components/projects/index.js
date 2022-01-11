b4803aa96724fa28a959c5a5ed767d51bfa5197b
shbhm00 <shbhm00@gmail.com>
2022-01-11 18:20:38 +0530

    project section

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -1,10 +1,32 @@
 import React from "react";
-import Timeline from "../timeline/index";
+import "./Project.css";
+
+function Card() {
+  return (
+    <div>
+      <div className="card"></div>
+    </div>
+  );
+}
 
 const projects = () => {
   return (
-    <div>
-      <Timeline />
+    <div className="wrapper">
+      <div>
+        <h1 style={{ fontSize: 50, color: "white", marginTop: 100 }}>
+          Projects
+        </h1>
+        <div className="card-wrapper">
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <div className="card-wrapper">
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
     </div>
   );
 };
